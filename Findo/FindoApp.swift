40ac19f9f8d01ff45fe1b689bc68e010659d586f
Mercen <mercen@mercen.net>
2024-05-14 14:46:21 +0900

    Initial Commit

---
+++ Findo/FindoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindoApp.swift
+//  Findo
+//
+//  Created by Mercen on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FindoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
